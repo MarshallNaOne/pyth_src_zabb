192d3e7ba3d6ad500882133679d1dbd9b8e362fe
MarshallNaOne <admin@vkhlebnikov.ru>
2022-08-23 16:30:52 +0000

    Add change pm2.5 or pm10 print

--- pm_zabbix.py
+++ pm_zabbix.py
@@ -6,10 +6,17 @@ import sys
 
 
 ip = sys.argv[1]
+change = sys.argv[2]
 
 url = "http://"+ip+"/data.json"
 response = urlopen(url)
 data_json = json.loads(response.read())
+pm10 = data_json['sensordatavalues'][0]['value']
+pm2 = data_json['sensordatavalues'][1]['value']
 
-print(data_json['sensordatavalues'][0]['value'])
+if change == '2':
+    print(pm2)
+elif change == '10':
+    print(pm10)
+
 
